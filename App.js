6ac9073e3f6cfbd26e17de55efc9d59b56ce51b4
Nicolas Talavera <nicolas@tamades.com>
2023-02-17 16:27:24 +0100

    with autenticator

--- App.js
+++ App.js
@@ -2,16 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Amplify } from 'aws-amplify'
 import awsconfig from './src/aws-exports'
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 
 Amplify.configure(awsconfig)
 
 
 export default function App() {
   return (
+    <Authenticator.Provider>
+     <Authenticator
+        components={{
+          SignUp: ({ fields, ...props }) => (
+            <Authenticator.SignUp
+              {...props}
+              fields={[
+                ...fields,
+                {
+                  name: "address",
+                  label: 'address',
+                  type: 'string',
+                  placeholder: 'Enter your address',
+                },
+              ]}
+            />
+          ),
+        }}
+      >
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </Authenticator>
+    </Authenticator.Provider>
   );
 }
 
